42a92033311fca9777570b581aee362d854f58b3
Emil Terhoven <eterhoven@gmail.com>
2021-04-06 17:23:20 +0200

    Modified python file

--- hello.py
+++ hello.py
@@ -1,21 +1,6 @@
-def start_game():
+def hello_world():
+    print("Hello lovely world")
 
-    game_on = True
+hello_world()
 
-    while game_on:
-
-        x = input("Would you like to play? Y or N")
-       
-        if x.lower == 'y':
-            print("We have succesfully started the game")
-            game_on = False
-        elif x.lower == 'n':
-            print("Okay sure, we will leave that alone")
-            game_on = False
-        else:
-            print("That was not a valid selection")
-
-print("Welcome to the new python file")
-
-start_game()
 
